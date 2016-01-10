43125d656e3398fcec0969235721dde9ee77c6e2
Ernest Fakudze <Ernest Fakudze>
2016-01-10 20:42:30 +0000

    Removes the exclusion of Scripts folder from the .gitignore file.

---
+++ SignalRExperiments/Scripts/main.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    var realTimeDataHub = $.connection.realtimeDataHub;
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+    realTimeDataHub.client.newServerTime = function (newTime) {
+        $('#timeDisplay').html(newTime);
+    };
+}());
---
+++ SignalRExperiments/Scripts/serverJobStatus.js
@@ -0,0 +1,29 @@
+﻿(function () {
+
+    var jobsHub = $.connection.jobsHub;
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+    jobsHub.client.newJobStatus = function (jobName, jobStatus) {
+        var displayMessage = 'Job with name ' + jobName + ' has status ' + jobStatus;
+        $('#jobStatusDisplay').html(displayMessage);
+    };
+
+    $(function () {
+        $('#btnStartJob').on('click', function (evt) {
+            evt.preventDefault();
+            $.post("/Hubs/ShowJobPercentageCompleted",
+            {
+                jobStart: $('#btnStartJob').val()
+            },
+            function (data, status) {
+               // alert("Data: " + data + "\nStatus: " + status);
+            });
+
+           
+        });
+    });
+
+
+
+}());
